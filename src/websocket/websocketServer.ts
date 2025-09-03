9560b8b8d1914b43b3d8b33ad9b664979d5b90aa
Pranav <ppranavas@gmail.com>
2025-09-03 11:58:31 -0400

    Update v1.0

--- src/websocket/websocketServer.ts
+++ src/websocket/websocketServer.ts
@@ -18,28 +18,66 @@
 
 
 
+// import http from "http";
+// import { WebSocketServer } from "ws";
+// import { handleConnection } from "./connectionManager";
+
+// export const initServer = (server: http.Server) => {
+//   const wss = new WebSocketServer({ noServer: true });
+
+//   server.on("upgrade", (req, socket, head) => {
+//     try {
+//       const url = new URL(req.url || "/", `http://${req.headers.host}`);
+//       if (url.pathname !== "/ws") {
+//         socket.destroy();
+//         return;
+//       }
+//       wss.handleUpgrade(req, socket, head, (ws) => {
+//         wss.emit("connection", ws, req);
+//       });
+//     } catch {
+//       socket.destroy();
+//     }
+//   });
+
+//   wss.on("connection", (ws) => handleConnection(ws));
+//   console.log("WebSocket upgrade route active at /ws");
+// };
+
+
+
+
+
 import http from "http";
 import { WebSocketServer } from "ws";
 import { handleConnection } from "./connectionManager";
 
 export const initServer = (server: http.Server) => {
-  const wss = new WebSocketServer({ noServer: true });
+  // Accept WS upgrades only on /ws
+  const wss = new WebSocketServer({ server, path: "/ws" });
 
-  server.on("upgrade", (req, socket, head) => {
-    try {
-      const url = new URL(req.url || "/", `http://${req.headers.host}`);
-      if (url.pathname !== "/ws") {
-        socket.destroy();
-        return;
-      }
-      wss.handleUpgrade(req, socket, head, (ws) => {
-        wss.emit("connection", ws, req);
-      });
-    } catch {
-      socket.destroy();
-    }
+  wss.on("connection", (ws, req) => {
+    console.log("WS client connected:", req.socket.remoteAddress);
+    handleConnection(ws);
   });
 
-  wss.on("connection", (ws) => handleConnection(ws));
-  console.log("WebSocket upgrade route active at /ws");
+  // Optional: keep-alive (helps prevent idle disconnects)
+  const interval = setInterval(() => {
+    for (const client of wss.clients) {
+      // @ts-ignore add a simple heartbeat flag
+      if ((client as any).isAlive === false) { client.terminate(); continue; }
+      (client as any).isAlive = false;
+      client.ping();
+    }
+  }, 30000);
+
+  wss.on("connection", (ws) => {
+    // @ts-ignore heartbeat flag
+    (ws as any).isAlive = true;
+    ws.on("pong", () => { /* @ts-ignore */ (ws as any).isAlive = true; });
+  });
+
+  wss.on("close", () => clearInterval(interval));
+
+  console.log("WebSocket listening at path /ws");
 };
